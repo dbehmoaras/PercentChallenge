f96d769bb84166a41dc57ada85c7fec90a87cee5
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-07-06 21:05:44 -0400

    file-tree ready

---
+++ src/API/PointOfSaleTerminal.java
@@ -0,0 +1,17 @@
+package API;
+
+public class PointOfSaleTerminal {
+
+
+	public String testString = "POINT OF SALE TEST STRING PRINTED";
+
+
+	public PointOfSaleTerminal() {
+
+	}
+
+
+	public void printTestString() {
+		System.out.println(testString);
+	}
+}
