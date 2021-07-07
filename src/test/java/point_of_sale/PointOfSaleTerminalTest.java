382d371b4589a8deaee5978a156f3dd52f91079e
David Behmoaras <dbehmoaras@gmail.com>
2021-07-07 16:03:08 -0400

    committing target

--- src/test/java/point_of_sale/PointOfSaleTerminalTest.java
+++ src/test/java/point_of_sale/PointOfSaleTerminalTest.java
@@ -6,20 +6,22 @@ import org.junit.jupiter.api.*;
 
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class pointOfSaleTest {
+public class PointOfSaleTerminalTest {
 	@Test
-	@DisplayName("*****TESTER ONE*****")
 	public void testerOne() {
+		System.out.println("*****TESTER ONE PRINT*****");
 		assertEquals(7, 3+4, "***** 1. testerOne *****");
 	}
 
 	@Test
 	public void testerTwo() {
 		assertEquals(8, 3 + 5, "***** 2. testerTwo *****");
+		System.out.println("*****TESTER TWO PRINT*****");
 	}
 
 	@Test
 	public void testerThree() {
+		System.out.println("*****TESTER THREE PRINT*****");
 		assertEquals(9, 3 + 6, "***** 3. testerThree *****");
 	}
 }
